566848dab542c7851745038e1d1db25e62b78ab3
anagri <127566+anagri@users.noreply.github.com>
2024-03-13 06:15:43 +0530

    [Amir] having main not initialize tokio runtime, starting async runtime only when it is needed

--- bodhiserver/src/lib.rs
+++ bodhiserver/src/lib.rs
@@ -1,5 +1,2 @@
 pub mod server;
-pub use server::{
-  build_server, port_from_env_vars, shutdown_signal, ServerHandle, DEFAULT_HOST, DEFAULT_PORT,
-  DEFAULT_PORT_STR,
-};
+pub use server::*;
--- bodhiserver/src/main.rs
+++ bodhiserver/src/main.rs
@@ -26,34 +26,43 @@ enum Command {
   },
 }
 
-#[tokio::main]
-pub async fn main() {
+pub fn main() {
   dotenv::dotenv().ok();
   tracing_subscriber::registry()
     .with(tracing_subscriber::fmt::layer())
     .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
     .init();
-  let result = start().await;
+  let result = start();
   if let Err(err) = result {
     tracing::warn!(err = ?err, "application exited with error");
     std::process::exit(1);
   }
 }
 
-async fn start() -> anyhow::Result<()> {
+fn start() -> anyhow::Result<()> {
   let cli = Cli::parse();
   match cli.command {
-    Command::Serve { host, port } => launch_server(host, port).await,
+    Command::Serve { host, port, .. } => serve(host, port),
   }
 }
 
-async fn launch_server(host: Option<String>, port: Option<u16>) -> anyhow::Result<()> {
+fn serve(host: Option<String>, port: Option<u16>) -> anyhow::Result<()> {
   let host = host.unwrap_or_else(|| String::from(DEFAULT_HOST));
   let port = port.unwrap_or_else(|| port_from_env_vars(std::env::var(ENV_BODHISERVER_PORT)));
+  let runtime = tokio::runtime::Builder::new_multi_thread()
+    .enable_all()
+    .build();
+  match runtime {
+    Ok(runtime) => runtime.block_on(async move { start_server(host, port).await }),
+    Err(err) => Err(anyhow::Error::new(err)),
+  }
+}
+
+async fn start_server(host: String, port: u16) -> anyhow::Result<()> {
   let ServerHandle { server, shutdown } = build_server(host, port).await?;
   let server_join = tokio::spawn(async move {
     match server.await {
-      Ok(_) => Ok(()),
+      Ok(()) => Ok(()),
       Err(err) => {
         tracing::error!(err = ?err, "server encountered an error");
         Err(err)
