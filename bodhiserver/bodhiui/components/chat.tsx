2a5eab8838cb35d9d89cf1a4c79c748a5d8685dc
anagri <127566+anagri@users.noreply.github.com>
2024-04-18 13:55:39 +0530

    [Amir] added chat panel

--- bodhiserver/bodhiui/components/chat.tsx
+++ bodhiserver/bodhiui/components/chat.tsx
@@ -1,6 +1,7 @@
 import { type Message, useChat } from "ai/react";
 import { ChatList } from "@/components/chat-list";
 import { EmptyScreen } from "@/components/empty-screen";
+import { ChatPanel } from "@/components/chat-panel";
 
 
 export interface ChatProps {
@@ -9,9 +10,19 @@ export interface ChatProps {
 }
 
 export function Chat({ id, initialMessages }: ChatProps) {
-  const { messages } = useChat({ initialMessages, id });
+  const { messages, input, setInput, isLoading, append, reload } = useChat({ initialMessages, id });
   return (<div className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
     {messages.length ? <ChatList messages={messages} /> : <EmptyScreen />}
+    <ChatPanel
+      id={id}
+      isLoading={isLoading}
+      stop={stop}
+      append={append}
+      reload={reload}
+      messages={messages}
+      input={input}
+      setInput={setInput}
+    />
   </div>)
 }
 
--- bodhiserver/bodhiui/components/ui/icons.tsx
+++ bodhiserver/bodhiui/components/ui/icons.tsx
@@ -183,3 +183,59 @@ export function IconUsers({ className, ...props }: React.ComponentProps<'svg'>) 
     </svg>
   )
 }
+
+export function IconStop({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88 88.1 88.1 0 0 1-88 88Zm24-120h-48a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h48a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8Zm-8 48h-32v-32h32Z" />
+    </svg>
+  )
+}
+
+export function IconRefresh({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path d="M197.67 186.37a8 8 0 0 1 0 11.29C196.58 198.73 170.82 224 128 224c-37.39 0-64.53-22.4-80-39.85V208a8 8 0 0 1-16 0v-48a8 8 0 0 1 8-8h48a8 8 0 0 1 0 16H55.44C67.76 183.35 93 208 128 208c36 0 58.14-21.46 58.36-21.68a8 8 0 0 1 11.31.05ZM216 40a8 8 0 0 0-8 8v23.85C192.53 54.4 165.39 32 128 32c-42.82 0-68.58 25.27-69.66 26.34a8 8 0 0 0 11.3 11.34C69.86 69.46 92 48 128 48c35 0 60.24 24.65 72.56 40H168a8 8 0 0 0 0 16h48a8 8 0 0 0 8-8V48a8 8 0 0 0-8-8Z" />
+    </svg>
+  )
+}
+
+export function IconArrowDown({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path d="m205.66 149.66-72 72a8 8 0 0 1-11.32 0l-72-72a8 8 0 0 1 11.32-11.32L120 196.69V40a8 8 0 0 1 16 0v156.69l58.34-58.35a8 8 0 0 1 11.32 11.32Z" />
+    </svg>
+  )
+}
+
+export function IconArrowElbow({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn('size-4', className)}
+      {...props}
+    >
+      <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z" />
+    </svg>
+  )
+}
