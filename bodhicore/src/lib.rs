fe0378e4d9488c521a48356a2c63b786dffe661b
anagri <127566+anagri@users.noreply.github.com>
2024-06-09 19:02:10 +0530

    [Amir] removing dangling code

--- bodhicore/src/lib.rs
+++ bodhicore/src/lib.rs
@@ -8,12 +8,13 @@ pub mod objs;
 pub mod server;
 pub mod service;
 mod shared_rw;
+#[cfg(test)]
+mod test_utils;
 mod tokenizer_config;
 mod utils;
+
 // TODO: remove exposing of cli methods, rename cli to command package
 pub use cli::*;
 pub use error::BodhiError;
 pub use objs::Repo;
 pub use shared_rw::{ContextError, SharedContextRw, SharedContextRwFn};
-#[cfg(test)]
-mod test_utils;
