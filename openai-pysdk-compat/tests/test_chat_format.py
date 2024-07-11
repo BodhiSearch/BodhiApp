711273ae5c34eb37a10915afdf237a84414109df
anagri <127566+anagri@users.noreply.github.com>
2024-07-11 18:18:15 +0530

    [Amir] response_format with schema for llama.cpp

--- openai-pysdk-compat/tests/test_chat_format.py
+++ openai-pysdk-compat/tests/test_chat_format.py
@@ -1,6 +1,9 @@
 import json
+from typing import List
 
 import pytest
+from deepdiff import DeepDiff
+from pydantic import BaseModel, TypeAdapter
 
 from .common import GPT_MODEL, LLAMA3_MODEL, mark_bodhi, mark_openai
 
@@ -53,3 +56,66 @@ async def test_chat_async_format_simple(client, model):
   except json.JSONDecodeError:
     pytest.fail(f"Invalid JSON:\n{content}")
   assert expected == json_obj
+
+
+class Champion(BaseModel):
+  yr: int
+  first_name: str
+  last_name: str
+
+
+class Champions(BaseModel):
+  champions: List[Champion]
+
+
+schema = TypeAdapter(Champions).json_schema()
+
+winner_expected = [
+  {"yr": 2002, "first_name": "Lleyton", "last_name": "Hewitt"},
+  {"yr": 2003, "first_name": "Roger", "last_name": "Federer"},
+  {"yr": 2004, "first_name": "Roger", "last_name": "Federer"},
+]
+prompt_wimblendon = f"""provide list of wimblendon men's single winners from 2002 to 2004.
+You respond in JSON format with each entry using the following schema:
+```
+{json.dumps(schema, indent=2)}
+```
+"""
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize(
+  ["client", "model", "response_format", "expected"],
+  [
+    pytest.param(
+      "openai", GPT_MODEL, {"type": "json_object"}, {"champions": winner_expected}, id="openai", **mark_openai()
+    ),
+    pytest.param(
+      "bodhi",
+      LLAMA3_MODEL,
+      {"type": "json_object", "schema": schema},
+      {"champions": winner_expected, "title": "Wimbledon Men's Singles Winners (2002-2004)", "type": "array"},
+      id="bodhi",
+      **mark_bodhi(),
+    ),
+  ],
+  indirect=["client"],
+)
+def test_chat_format_schema(client, model, response_format, expected):
+  response = client.chat.completions.create(
+    model=model,
+    messages=[
+      {
+        "role": "system",
+        "content": "You are a helpful assistant that generates the output in the given json schema format",
+      },
+      {
+        "role": "user",
+        "content": prompt_wimblendon,
+      },
+    ],
+    response_format=response_format,
+  )
+  actual = json.loads(response.choices[0].message.content)
+  assert expected["champions"] == actual["champions"]
+  assert expected == actual
