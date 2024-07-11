54a9abe06bfdf25fe02a218bb76f8927ad5b2939
anagri <127566+anagri@users.noreply.github.com>
2024-07-11 16:02:49 +0530

    [Amir] removing redundant test

--- openai-pysdk-compat/tests/test_chat_format.py
+++ openai-pysdk-compat/tests/test_chat_format.py
@@ -1,7 +1,6 @@
 import json
 
 import pytest
-from deepdiff import DeepDiff
 
 from .common import GPT_MODEL, LLAMA3_MODEL, mark_bodhi, mark_openai
 
@@ -19,47 +18,6 @@ input_json_format = {
 }
 
 expected = {"firstName": "John", "lastName": "Doe", "age": 30}
-
-
-@pytest.mark.vcr
-@pytest.mark.parametrize(
-  "args",
-  [pytest.param(input_json_format, id="format_json", marks=pytest.mark.unmarked())],
-)
-def test_format_compare(openai_client, bodhi_client, args):
-  gpt_response = openai_client.chat.completions.create(model=GPT_MODEL, **args)
-  bodhi_response = bodhi_client.chat.completions.create(model=LLAMA3_MODEL, **args)
-  exclude_paths = [
-    "id",
-    "created",
-    "model",
-    "system_fingerprint",
-    "choices[0].message.content",
-  ]
-  diff = DeepDiff(gpt_response, bodhi_response, ignore_order=True, exclude_paths=exclude_paths)
-  expected_usage_diff = {
-    "root.usage.completion_tokens": {
-      "new_value": 28,
-      "old_value": 26,
-    },
-    "root.usage.prompt_tokens": {
-      "new_value": 0,
-      "old_value": 26,
-    },
-    "root.usage.total_tokens": {
-      "new_value": 28,
-      "old_value": 52,
-    },
-  }
-  content = bodhi_response.choices[0].message.content
-  try:
-    json_obj = json.loads(content)
-  except json.JSONDecodeError:
-    pytest.fail(f"Invalid JSON:\n{content}")
-  assert {"firstName": "John", "lastName": "Doe", "age": 30} == json_obj
-  # assert expected_usage_diff == diff.pop("values_changed") # TODO: implement
-  assert ["root.choices[0].model_fields_set['logprobs']"] == diff.pop("set_item_removed")  # TODO: implement
-  # assert {} == diff # TODO: implement
 
 
 @pytest.mark.vcr
