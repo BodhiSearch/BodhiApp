6b5aa008baf2d9ed6194d28a64e8a5af57d94860
anagri <127566+anagri@users.noreply.github.com>
2024-07-23 21:03:40 +0530

    [Amir] fixing dircpy logic of copying folders recursively

--- bodhicore/tests/utils/live_server_utils.rs
+++ bodhicore/tests/utils/live_server_utils.rs
@@ -14,6 +14,7 @@ pub fn copy_test_dir(src: &str, dst_path: &Path) {
   let src_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(src);
   CopyBuilder::new(src_path, dst_path)
     .overwrite(true)
+    .with_include_filter("")
     .run()
     .unwrap();
 }
