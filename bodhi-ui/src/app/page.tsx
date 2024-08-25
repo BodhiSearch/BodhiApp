27408ba78db2ba09f34ca55501db84823f97c26c
anagri <127566+anagri@users.noreply.github.com>
2024-08-25 21:03:29 +0530

    [Amir] setting up redirect to /ui for root page

--- bodhi-ui/src/app/page.tsx
+++ bodhi-ui/src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button"
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+  redirect('/ui');
 }
