87781ed2fda62100673fd92e758a17107ebbfeba
anagri <127566+anagri@users.noreply.github.com>
2024-10-10 20:18:38 +0530

    [Amir][intermediate] integrated localization with api_error to read messages from localization resource

--- crates/objs/src/test_utils/l10n.rs
+++ crates/objs/src/test_utils/l10n.rs
@@ -1,14 +1,37 @@
 use crate::FluentLocalizationService;
 use rstest::fixture;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 impl FluentLocalizationService {
   pub fn new_standalone() -> Self {
     Self::new()
+  }
+
+  pub fn get_instance() -> Arc<FluentLocalizationService> {
+    MOCK_LOCALIZATION_SERVICE
+      .read()
+      .unwrap()
+      .as_ref()
+      .unwrap()
+      .clone()
   }
 }
 
 #[fixture]
 pub fn localization_service() -> Arc<FluentLocalizationService> {
   Arc::new(FluentLocalizationService::new_standalone())
+}
+
+lazy_static::lazy_static! {
+  pub static ref MOCK_LOCALIZATION_SERVICE: Arc<RwLock<Option<Arc<FluentLocalizationService>>>> = Arc::new(RwLock::new(None));
+}
+
+pub fn set_mock_localization_service(service: Arc<FluentLocalizationService>) {
+  let mut mock = MOCK_LOCALIZATION_SERVICE.write().unwrap();
+  *mock = Some(service);
+}
+
+pub fn clear_mock_localization_service() {
+  let mut mock = MOCK_LOCALIZATION_SERVICE.write().unwrap();
+  *mock = None;
 }
