8c7b4cecdc8f6579c3d52d6794751608fcee289c
anagri <127566+anagri@users.noreply.github.com>
2024-10-11 16:40:48 +0530

    [Amir][intermediate] migrated routes_oai to separate module

--- crates/routes_oai/src/lib.rs
+++ crates/routes_oai/src/lib.rs
@@ -3,6 +3,8 @@ pub mod test_utils;
 #[cfg(all(not(feature = "test-utils"), test))]
 pub mod test_utils;
 
+mod routes_chat;
 mod routes_oai_models;
 
+pub use routes_chat::*;
 pub use routes_oai_models::*;
