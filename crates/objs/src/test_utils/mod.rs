fe83e6ef7dc2941165f0135f8bcedb56c33dd621
anagri <127566+anagri@users.noreply.github.com>
2024-10-11 07:05:00 +0530

    [Amir][intermediate] moving tracing setup to logs and optional

--- crates/objs/src/test_utils/mod.rs
+++ crates/objs/src/test_utils/mod.rs
@@ -5,6 +5,7 @@ mod error;
 mod hf;
 mod io;
 mod l10n;
+mod logs;
 mod objs;
 
 pub use bodhi::*;
@@ -12,15 +13,7 @@ pub use error::*;
 pub use hf::*;
 pub use io::*;
 pub use l10n::*;
+pub use logs::*;
 pub use objs::*;
 
 pub static SNAPSHOT: &str = "5007652f7a641fe7170e0bad4f63839419bd9213";
-
-#[ctor::ctor]
-fn init_tracing() {
-  use tracing_subscriber::fmt::format::FmtSpan;
-  tracing_subscriber::fmt()
-    .with_test_writer()
-    .with_span_events(FmtSpan::FULL)
-    .init();
-}
