43754d22d16a1f0327fe9f6b6f9aec913c7b67b5
anagri <127566+anagri@users.noreply.github.com>
2024-10-11 23:22:06 +0530

    [Amir][intermediate] added routes_ui to routes_app

--- crates/routes_app/src/lib.rs
+++ crates/routes_app/src/lib.rs
@@ -8,9 +8,11 @@ mod routes_dev;
 mod routes_login;
 mod routes_pull;
 mod routes_setup;
+mod routes_ui;
 
 pub use routes_create::*;
 pub use routes_dev::*;
 pub use routes_login::*;
 pub use routes_pull::*;
 pub use routes_setup::*;
+pub use routes_ui::*;
