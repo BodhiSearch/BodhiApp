6bd429cec28eab6f7de9f0ecddad8fb907799fe9
anagri <127566+anagri@users.noreply.github.com>
2024-10-12 17:52:55 +0530

    [Amir][intermediate] removing server module

--- crates/server/src/test_utils/bindings.rs
+++
@@ -1,9 +0,0 @@
-use llama_server_bindings::{bindings::llama_server_disable_logging, disable_llama_log};
-
-#[allow(unused)]
-pub fn disable_test_logging() {
-  disable_llama_log();
-  unsafe {
-    llama_server_disable_logging();
-  }
-}
