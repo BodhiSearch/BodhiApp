0baea50db31175b043bf207b16511ca1783f8b9b
anagri <127566+anagri@users.noreply.github.com>
2024-10-24 09:19:03 +0000

    [Amir] setting up devcontainer right, having dockerfile for rust

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -e
 
+# https://github.com/devcontainers/features/pull/754/files
+sudo chown -R "vscode:vscode" "${RUSTUP_HOME}" "${CARGO_HOME}" && \
+    find "${RUSTUP_HOME}" -type d -print0 | xargs -n 1 -0 chmod g+s && \
+    find "${CARGO_HOME}" -type d -print0 | xargs -n 1 -0 chmod g+s
+
 # cargo installs
-bash /home/vscode/dotfiles/install_devcontainer.sh && pre-commit install
+bash /home/vscode/dotfiles/install_devcontainer.sh
+export PRE_COMMIT_ALLOW_NO_CONFIG=1
+export NEXT_TELEMETRY_DISABLED=1
+cd /workspaces/BodhiApp/llamacpp-sys && pre-commit install
+cd /workspaces/BodhiApp/llamacpp-sys/llama.cpp && pre-commit install
+cd /workspaces/BodhiApp
