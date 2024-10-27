74e1d2afb1287564ce75cda7b2d9423849dc538e
anagri <127566+anagri@users.noreply.github.com>
2024-10-27 12:34:39 +0000

    [Amir][temp] integrating app launch with bodhiui

--- crates/bodhiui/src-tauri/src/main.rs
+++ crates/bodhiui/src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(feature = "native")]
 fn main() {
-  app_lib::run();
+  app_lib::native::run();
+}
+
+#[cfg(not(feature = "native"))]
+fn main() {
+  println!("BodhiUI is not built with native support");
 }
