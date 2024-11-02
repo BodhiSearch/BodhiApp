d49b6f654231e8b93f84875ab2edb20691c8ae88
anagri <127566+anagri@users.noreply.github.com>
2024-11-02 11:55:30 +0530

    [Amir] fixing for windows

--- crates/llamacpp_rs/tests/utils/callbacks.rs
+++ crates/llamacpp_rs/tests/utils/callbacks.rs
@@ -6,7 +6,7 @@ pub extern "C" fn test_callback(
   size: usize,
   userdata: *mut c_void,
 ) -> usize {
-  let slice = unsafe { slice::from_raw_parts(contents, size) };
+  let slice = unsafe { slice::from_raw_parts(contents as *const u8, size) };
   let input_str = match std::str::from_utf8(slice) {
     Ok(s) => s,
     Err(_) => return 0,
