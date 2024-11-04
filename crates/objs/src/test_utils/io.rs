98e5de0e7a536135160850a91133422a7e1f2e14
anagri <127566+anagri@users.noreply.github.com>
2024-11-04 07:44:19 +0000

    [Amir] using fs_extra to copy instead of dircpy, to see if working for windows

--- crates/objs/src/test_utils/io.rs
+++ crates/objs/src/test_utils/io.rs
@@ -1,11 +1,17 @@
 use dircpy::CopyBuilder;
+use fs_extra::dir::{copy, CopyOptions};
 use std::path::Path;
+
+static COPY_OPTIONS: CopyOptions = CopyOptions {
+  overwrite: true,
+  skip_exist: false,
+  copy_inside: true,
+  content_only: false,
+  buffer_size: 64000,
+  depth: 0,
+};
 
 pub fn copy_test_dir(src: &str, dst_path: &Path) {
   let src_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(src);
-  CopyBuilder::new(src_path, dst_path)
-    .overwrite(true)
-    .with_include_filter("")
-    .run()
-    .unwrap();
+  copy(src_path, dst_path, &COPY_OPTIONS).unwrap();
 }
