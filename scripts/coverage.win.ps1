80886742829375147e86491f93b04598f7a8c6ba
anagri <127566+anagri@users.noreply.github.com>
2024-12-25 19:45:31 +0530

    [Amir] fixing build failures, first building llama_server_proc

--- scripts/coverage.win.ps1
+++ scripts/coverage.win.ps1
@@ -1,5 +1,9 @@
 # scripts/coverage.win.ps1
 
+$cmd = "cargo build -p llama_server_proc"
+Write-Host "Executing command: $cmd"
+Invoke-Expression $cmd
+
 $packages = (cargo metadata --no-deps --format-version 1 |
   ConvertFrom-Json).packages.name |
   ForEach-Object { "-p $_" }
